e5e6e2012744559115c9082a6762d1737e529200
Muhammad Awais <ows4444@gmail.com>
2024-09-30 00:42:57 +0500

    make nested dynamic path

--- app/dashboard/[slug]/[child-slug]/page.tsx
+++ app/dashboard/[slug]/[child-slug]/page.tsx
@@ -1,8 +1,7 @@
 import type React from 'next';
-import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 
 import { H2 } from '@components/core/atoms';
 
-export default async function Page({ params }: Readonly<{ params: Params }>) {
+export default async function Page({ params }: Readonly<{ params: any }>) {
 	return <H2 content={`/${params.slug}/${params['child-slug']}`} />;
 }
